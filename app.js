7f5728af863dfa9a21bf277bc9eda63fbea185cd
Aditya Raj <adityaraj34@icloud.com>
2026-02-15 13:44:37 +0530

    all instal

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const express = require("express")
+const socket =require("socket.io")
+const http =require("http")
+const {chess} =require("chess.js");
+
+const app =express();
+const server=http.createServer(app);
+
+const io = socket(server);
+
+const chess = new chess();
+let players ={}
+let currentPlayer="w";
+
+app.set("view wngine ","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
